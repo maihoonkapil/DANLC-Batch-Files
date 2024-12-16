8c404774c4648fd550921dbf74c7c8105212c1ff
Kapil Khatri <162281911+maihoonkapil@users.noreply.github.com>
2024-12-16 11:47:03 +0530

    python code

---
+++ Pattern.py
@@ -0,0 +1,15 @@
+number = int(input("Enter the number: "))
+
+for i in range(1, number + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    for k in range(i - 1, 0, -1):
+        print(k, end="")
+    print()
+
+for i in range(number - 1, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    for k in range(i - 1, 0, -1):
+        print(k, end="")
+    print()
